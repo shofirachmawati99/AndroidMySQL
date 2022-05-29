181edc7bda18c174569c071cdaeefc327e62b0c5
Arfies27 <salsabilhusna01@gmail.com>
2022-05-29 16:28:05 +0700

    upload project

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/shofi/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.mhs.shofi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
